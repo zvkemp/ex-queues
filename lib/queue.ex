76423c8d39b4096f315686c84e9233498e55e851
Zach Kemp <zvkemp@gmail.com>
2017-10-07 10:26:21 -0700

    additional tests

--- lib/queue.ex
+++ lib/queue.ex
@@ -1,6 +1,6 @@
 defmodule Queue do
   @moduledoc """
-  Documentation for Queue.
+  blocking FIFO queue agent (wrapper around erlang's :queue
   """
 
   use GenServer
@@ -9,6 +9,7 @@ defmodule Queue do
     GenServer.start_link(__MODULE__, [], opts)
   end
 
+  @impl true
   def init(_arg) do
     {:ok, {:queue.new(), :queue.new()}}
   end
@@ -18,46 +19,68 @@ defmodule Queue do
   end
 
   def pop(q, timeout \\ :infinity) do
-    GenServer.call(q, :pop, timeout)
-  end
-
-  def handle_call({:push, val}, from, {state, {[], []} = waiters}) do
-    new_state = :queue.in(val, state)
-    {:reply, :ok, {new_state, waiters}} |> IO.inspect
-  end
-
-  def handle_call({:push, val}, from, {state, waiters}) do
-    {{:value, waiter}, new_waiters} = :queue.out(waiters)
-    # require IEx; IEx.pry
-    GenServer.reply(waiter, val)
-    {:noreply, {state, new_waiters}} |> IO.inspect
-  end
-
-
-  def handle_call(:pop, from, {state, waiters}) do
-    case :queue.out(state) do
-      {{:value, val}, new_state} -> {:reply, val, {new_state, waiters}}
-      {:empty, new_state} -> wait_for_value(from, {new_state, waiters})
+    case GenServer.call(q, :pop, timeout) do
+      {:ok, val} -> val
+      {:wait, ref} ->
+        receive do
+          {^ref, val} -> val
+        after
+          timeout -> unregister_pending(q, ref)
+                     {:error, :timeout}
+        end
     end
   end
 
-  defp wait_for_value(waiter, {state, waiters}) do
-    {:noreply, {state, :queue.in(waiter, waiters)}}
+  def stream(q) do
+    Stream.repeatedly(fn -> Queue.pop(q) end)
   end
 
-  def demo_t do
-    {:ok, q} = Queue.start_link
-    spawn(fn ->
-      IO.puts("task start")
-      Process.sleep(2050)
-      IO.puts("task sleep finish")
-      Queue.push(q, :foo)
-    end)
+  def unregister_pending(q, ref) do
+    GenServer.call(q, {:unregister_pending, ref})
+  end
 
-    GenServer.call(q, :pop)
+  @impl true
+  def handle_call({:unregister_pending, ref}, {pid, _}, {queue, pending}) do
+    # Filters on original call ref and same pid
+    new_pending = :queue.filter(fn
+      {^pid, ^ref} -> false
+      _ -> true end,
+      pending)
+    {:reply, :ok, {queue, new_pending}}
+  end
+
+  # Push, no one is waiting
+  @impl true
+  def handle_call({:push, val}, _from, {state, {[], []} = pending}) do
+    new_state = :queue.in(val, state)
+    {:reply, :ok, {new_state, pending}}
+  end
+
+  # Push, and notify first pending pop
+  @impl true
+  def handle_call({:push, val}, _from, {state, pending}) do
+    {{:value, {pid, ref}}, new_pending} = :queue.out(pending)
+    Process.send(pid, {ref, val}, [])
+    {:reply, :ok, {state, new_pending}}
+  end
+
+  @impl true
+  def handle_call(:pop, from, {state, pending}) do
+    case :queue.out(state) do
+      {{:value, val}, new_state} -> {:reply, {:ok, val}, {new_state, pending}}
+      {:empty, new_state} -> wait_for_value(from, {new_state, pending})
+    end
+  end
+
+  defp wait_for_value({_pid, ref} = from, {state, pending}) do
+    {:reply, {:wait, ref}, {state, :queue.in(from, pending)}}
+  end
+
+  # NOTE: adding this as an additional push mechanism to support sending messages generically;
+  # has been useful for dev and testing environments.
+  @impl true
+  def handle_info({:push, val}, state) do
+    {:reply, :ok, new_state} = handle_call({:push, val}, nil, state)
+    {:noreply, new_state}
   end
 end
-
-# with a timeout:
-#
-# ----------*
--- test/queue_test.exs
+++ test/queue_test.exs
@@ -1,8 +1,91 @@
 defmodule QueueTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: false
   doctest Queue
 
-  test "greets the world" do
-    assert Queue.hello() == :world
+  test "basic sync usage" do
+    {:ok, q} = Queue.start_link
+
+    Queue.push(q, 1)
+    Queue.push(q, 2)
+    Queue.push(q, 3)
+    assert Queue.pop(q) == 1
+    assert Queue.pop(q) == 2
+    Queue.push(q, 4)
+    assert Queue.pop(q) == 3
+    assert Queue.pop(q) == 4
+  end
+
+  test "with infinite pop timeout" do
+    {:ok, q} = Queue.start_link
+
+    spawn fn ->
+      Process.sleep(200)
+      Queue.push(q, :foo)
+    end
+
+    assert Queue.pop(q) == :foo
+  end
+
+  test "messages are preserved on pop timeout" do
+    {:ok, q} = Queue.start_link
+
+    spawn fn ->
+      Process.sleep(300)
+      Queue.push(q, :foo)
+    end
+
+    assert Queue.pop(q, 50) == {:error, :timeout}
+    assert Queue.pop(q, 50) == {:error, :timeout}
+    assert Queue.pop(q, 50) == {:error, :timeout}
+    assert Queue.pop(q, 500) == :foo
+  end
+
+  test "streaming" do
+    {:ok, q} = Queue.start_link
+    Task.start fn ->
+      Stream.interval(30)
+      |> Stream.each(fn (i) -> Queue.push(q, i) end)
+      |> Stream.take(10)
+      |> Stream.run
+    end
+
+    assert (0..9) |> Enum.into([]) == Queue.stream(q) |> Enum.take(10)
+  end
+
+  test "multiple pending pops" do
+    {:ok, q} = Queue.start_link
+    tasks = Stream.repeatedly(fn ->
+      :timer.sleep(15) # ensure order
+      Task.async(fn -> Queue.pop(q) end)
+    end)
+    |> Enum.take(5)
+
+    :timer.sleep(15 * 6) # ensure tasks have started
+
+    # 10 processes are waiting on the queue
+    {{[], []}, {pa, pb}} = :sys.get_state(q)
+    pending = pa ++ pb
+    assert Enum.count(pending) == 5
+
+    for x <- (1..5), do: Queue.push(q, x)
+    # order not guaranteed because of task synch issues
+    assert (1..5) |> Enum.into([]) == tasks |> Enum.map(&Task.await/1)
+  end
+
+  test "dead pending pops" do
+    {:ok, q} = Queue.start_link
+    t1 = Task.async fn -> Queue.pop(q) end
+    t2 = Task.async fn -> Queue.pop(q) end
+    t3 = Task.async fn -> Queue.pop(q) end
+
+    Task.shutdown(t1)
+    :timer.sleep(15)
+
+    Queue.push(q, 1)
+    Queue.push(q, 2)
+    Queue.push(q, 3)
+    # first push is sent to t2
+    assert 1 == t2 |> Task.await
+    assert 2 == t3 |> Task.await
   end
 end
